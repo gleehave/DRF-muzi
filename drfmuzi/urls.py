2e4682a0ff7ffd90988888c45328b028975a887f
gleehave <gleehave@gmail.com>
2022-05-25 17:08:55 +0900

    [add] 프로젝트 URL 설정

--- drfmuzi/urls.py
+++ drfmuzi/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('user/', include('users.urls')),
+    path('cart/', include('carts.urls')),
+    path('product/', include('products.urls')),
+    path('wishlist/', include('wishlists.urls')),
 ]
