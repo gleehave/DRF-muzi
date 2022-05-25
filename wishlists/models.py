9f7aa045b5f6736b58f4118c72694373158ec1fa
gleehave <gleehave@gmail.com>
2022-05-25 16:59:58 +0900

    [add] wishlist model 작성 및 migrate

--- wishlists/models.py
+++ wishlists/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+from core.timestamp import TimeStampedModel
+
+
+class Wishlist(TimeStampedModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='wishlist')
+    product_option = models.ForeignKey('products.ProductOption', on_delete=models.CASCADE, related_name='wishlist')
+
+    class Meta:
+        db_table = 'wishlists'
