5c0c2c8b4f02c7f8f609b85c55836d70d11d412b
gleehave <gleehave@gmail.com>
2022-05-25 23:17:40 +0900

    [fix] Custom auth.User Model (with migrate)

--- accounts/models.py
+++ accounts/models.py
@@ -30,7 +30,7 @@ class UserManager(BaseUserManager):
 
 class User(AbstractBaseUser, PermissionsMixin, TimeStampedModel):
     username     = models.CharField(max_length=254)
-    email        = models.EmailField(max_length=200)
+    email        = models.EmailField(max_length=200, unique=True)
     phone_number = models.CharField(max_length=50)
 
     USERNAME_FIELD  = 'email'
--- carts/models.py
+++ carts/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -5,7 +6,7 @@ from core.timestamp import TimeStampedModel
 
 
 class Cart(TimeStampedModel):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=0)
     product_option = models.ForeignKey('products.ProductOption', on_delete=models.CASCADE, related_name='cart')
 
