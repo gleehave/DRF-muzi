9d014ec43c8aa891a501d86bf3259e971caf7f1c
gleehave <gleehave@gmail.com>
2022-05-26 23:31:40 +0900

    [add] User test code

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.urls         import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+class SignUpTestCase(APITestCase):
+    def test_post_signup(self):
+        data = {
+            "username"    : "testuser",
+            "email"       : "test@example.com",
+            "password"    : "password##1234",
+            "phone_number":"010-111-1111"
+        }
+        response = self.client.post(reverse('signup'), data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
