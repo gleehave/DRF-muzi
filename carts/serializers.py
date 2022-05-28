fc0379fa768f1493c8c98d27c478a68fc1cda084
gleehave <gleehave@gmail.com>
2022-05-28 20:03:25 +0900

    [add] cart

---
+++ carts/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework       import serializers
+from accounts.serializers import UserSerializer
+from products.serializers import ProductOptionSerializer
+
+from carts.models         import Cart
+
+class CartSerializer(serializers.ModelSerializer):
+    user = UserSerializer
+    product_option = ProductOptionSerializer
+
+    class Meta:
+        model = Cart
+        fields = '__all__'
--- carts/urls.py
+++ carts/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 
+from carts.views import CartAPIView
+
 urlpatterns = [
+    path('', CartAPIView.as_view(), name='cart'),
 ]
+
