4b5e225aec7c4e3f7f1825bb9a34d04f22cf158b
gleehave <gleehave@gmail.com>
2022-05-28 20:19:23 +0900

    [add] Cart APIView

--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,25 @@
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
+
+from core.logindecorator import login_decorator
 
 
 class CartAPIView(APIView):
-    pass
+    permission_classes = [IsAuthenticated]
+
+    @login_decorator
+    def post(self, request):
+        pass
+
+    @login_decorator
+    def get(self, request):
+        pass
+
+    @login_decorator
+    def put(self, request):
+        pass
+
+    @login_decorator
+    def delete(self, request):
+        pass
+
