450d00e76128cf650976429d4071d6d3f4f0f82a
gleehave <gleehave@gmail.com>
2022-05-29 19:18:11 +0900

    [add] product test

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from django.urls         import reverse
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from products.models import Product, Type, Category, Tag
+
+
+class ProductTestCase(APITestCase):
+    def setUp(self):
+        self.category = Category.objects.create(name='category1')
+        self.type = Type.objects.create(name='type1', thumbnail_image_url='1', category=self.category)
+        self.product = Product.objects.create(
+            name = 'product1',
+            price = 1000,
+            description = 'this is product1',
+            thumbnail_image_url = 'image1.png',
+            type = self.type
+        )
+
+    def test_get_product_list_apiview(self):
+        pass
