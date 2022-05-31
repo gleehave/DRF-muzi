1d33e2fbbea9e96cce478de5b7c54f2c1a14ea9b
gleehave <gleehave@gmail.com>
2022-05-31 23:09:33 +0900

    [add] Cart test

--- carts/tests.py
+++ carts/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from accounts.models import User
+from carts.models import Cart
+from products.models import ProductOption
+
+class ProductTestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='test', email='test@example.com', password='test1234@@', is_staff=True)
+        self.productoption = ProductOption.objects.create(product_id=1, size_id=1,color_id=1,stock=3)
+        self.cart = Cart.objects.create(user_id=self.user.id, quantity=1, product_option=self.productoption.id)
+
+    def test_get_cart_APIView(self):
+        response = self.client.get(f'http://127.0.0.1/cart/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
