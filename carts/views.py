1bd0fe0257a6e519a51037bcdc71678ced4d54ad
gleehave <gleehave@gmail.com>
2022-06-05 15:15:46 +0900

    [add] cart post

--- carts/views.py
+++ carts/views.py
@@ -1,21 +1,29 @@
-from rest_framework             import status
+from rest_framework import status, generics
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response    import Response
 from rest_framework.views       import APIView
 
 from carts.models        import Cart
-from carts.serializers   import CartSerializer
+from carts.serializers import GETCartSerializer, POSTCartSerializer
 from core.logindecorator import login_decorator
 from core.querydebugger  import query_debugger
+from products.models import ProductOption, Product, Size, Color
+from products.serializers import ProductOptionSerializer
 
 
-class CartAPIView(APIView):
+class CartAPIView(generics.GenericAPIView):
     permission_classes = [IsAuthenticated]
 
     @login_decorator
     @query_debugger
     def post(self, request):
-        pass
+        try:
+            serializer = POSTCartSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+        except:
+            return Response(serializer.errors)
 
     @login_decorator
     @query_debugger
@@ -23,16 +31,12 @@ class CartAPIView(APIView):
         try:
             user_id = request.user.id
             cart = Cart.objects.filter(user_id=user_id)
-            serializer = CartSerializer(cart, many=True)
+            serializer = GETCartSerializer(cart, many=True)
             return Response(serializer.data)
         except Cart.DoesNotExist:
             return Response({
                 'error': "Cart_does_not_exist"
             }, status=status.HTTP_400_BAD_REQUEST)
-
-    @login_decorator
-    def put(self, request):
-        pass
 
     @login_decorator
     def delete(self, request):
